6d3861bb2e621e4bf914fb7f8dc01687c96c2137
Alexey Aristov <aav@acm.org>
2025-04-07 11:13:53 +0000

    lecture 1

--- src/lecture.clj
+++ src/lecture.clj
@@ -1,3 +1,34 @@
 (ns lecture)
 
+; ~1930 Alonso Church - Lambda Calculus (lambda abstraction) (beta reduction)
+; ~1950 Jim McArthy - Lisp (LISt Processing)
+; ~1980 Common Lisp (ANSI)
+
+; ~2008 Clojure - very close to common lisp
+;   - functional, symbolic, not-pure (not like Haskell), dynamic typed
+;   - good for: meta programming
+;   - compiled -> JVM byte code, runs on JVM, Java interoperability
+;   - ClojureScript 99.5% Clojure, compiles to JavaScript
+; REPL
+; Read Eval Print Loop 
+; interactive facility 
+
+; VS Code (Calva Extension)  ([n]vim, Emacs, Helix)
+; 
+; 
+
+
+
+"hello world"
+
+123
+
+
+
+
+; 
+; 
+
+
+
 
