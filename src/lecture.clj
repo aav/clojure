19ce4395cd2a9804050c482de978d14832cd7215
Alexey Aristov <aav@acm.org>
2025-10-20 11:27:25 +0000

    lecture 02

--- src/lecture.clj
+++ src/lecture.clj
@@ -1,3 +1,112 @@
 (ns lecture)
 
+; Number
+; (java int)
+10
+
+; (java float)
+3.14
+
+; java.math??.BigDecimal java.lang.BigInteger
+; BigInteger
+10N
+
+; BigDecimal
+100.5M
+
+; String (java.lang.String)
+"Hello World"
+
+; Character
+; java: 'A'
+\A
+\Z
+
+; boolean 
+true
+false
+
+; NULL, null  is it a type??? :)
+nil
+
+; keyword
+:helloWorld ; java?
+:hello_world ; C, Rust
+:hello-world ; Clojure
+
+; symbol
+'hello-world
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+; Clojure -> JVM Byte Code
+
+
+
+
+
+
+
+
+
+; a = 10;
+;
+; 
+
+
+
+
+
+
+
+
+
 
