9b1a4ced39ec10cebb1f1c072b6e30993c95c615
Alexey Aristov <aav@acm.org>
2026-05-04 11:16:10 +0000

    Lecture 4 (04.05.26)

--- src/lecture.clj
+++ src/lecture.clj
@@ -122,6 +122,160 @@ nil
 
 '({1 "one" 2 "two" 3 "three"} 2)
 
+'symb
+(+ 2 3 4)
+
+; 1 + 3
+; + 1 3
+
+; 1 + 2 + 3, !flag, flag ? 1 : 3
+; f(1, 2, 3, ...)
+; Arity - number of parameters
+
+; function applications only!
+(+ 1 2 3 4 5 6 7 8 9 10)
+
+; 2 + 3 * 4 
+(+ 2 (* 3 4))
+
+; var: x
+; abs: λx.T
+; red: T N
+
+; var -> sumbols
+
+; abs: 
+(fn [x]
+  ; T
+  )
+
+
+; special form
+
+; Lisp -> Common Lisp (ANSI) -> Clojure
+; 1957
+
++
+
+((fn [x]
+   (+ x 1)) 2)
+
+((fn [abc]
+   abc) 3)
+
+; homoiconicity
+; 123 'sss' {aa: "x"} true  JSON
+; if function switch 
+
+(let [abc 3]
+  abc)
+
+; 1:1 
+
+((fn [abc]
+   abc) 3)
+
+((fn [a b]
+  (+ a b)) 2 3)
+
+; Curry transformation
+
+; Partial application
+
+((fn [a]
+  (fn [b]
+    (+ a b))) 2)
+
+(fn [b]
+  (+ 2 b))
+
+(+ 2 3)
+
+5
+
+(((fn [a]
+   (fn [b]
+     (+ a b))) 2) 3)
+
+(fn [a b]
+  (+ a b))
+
+; Haskell
+; myFunction 1 2 3
+; (((myFunction 1) 2) 3) 
+
+; Higher order function
+
+(partial + 1)
+
+(let [my-inc (partial + 1)]
+  (my-inc 4))
+
+((fn [my-inc]
+  (my-inc 4)) (partial + 1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
